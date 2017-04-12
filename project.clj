e5f3219d477b05600310ac9ce81e96b1f8cf96e1
OJIMA Ryoji <rojima1@gmail.com>
2017-04-12 19:47:41 +0900

    project name is changed.

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url          "http://example.com/FIXME"
   :license      {:name "Eclipse Public License"
                  :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure       "1.8.0"]
-                 [org.clojure/core.async    "0.3.442"]
-                 [com.tail-island/robosight "0.1.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure            "1.8.0"]
+                 [org.clojure/core.async         "0.3.442"]
+                 [com.tail-island/robosight-core "0.1.0-SNAPSHOT"]]
   :aot          :all
   :main         com.tail_island.robosight.Visualizer
   :plugins      [[lein-checkouts "1.1.0"]])
