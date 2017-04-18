dc73fc7999c18376779024e7b0e0a2c65534aeba
ROjima1 <rojima1@gmail.com>
2017-04-18 14:45:15 +0900

    version is fixed.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.tail-island/robosight-visualizer "0.1.0-SNAPSHOT"
+(defproject com.tail-island/robosight-visualizer "0.1.0"
   :description  "FIXME: write description"
   :url          "http://example.com/FIXME"
   :license      {:name "Eclipse Public License"
                  :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure            "1.8.0"]
                  [org.clojure/core.async         "0.3.442"]
-                 [com.tail-island/robosight-core "0.1.0-SNAPSHOT"]]
+                 [com.tail-island/robosight-core "0.1.0"]]
   :aot          :all
   :main         com.tail_island.robosight.Visualizer
   :plugins      [[lein-checkouts "1.1.0"]])
